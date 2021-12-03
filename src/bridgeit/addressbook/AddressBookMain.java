38568228c399f6713b396c2676136a1acf021f8b
Shark1906 <amanraj1906@gmail.com>
2021-12-03 15:42:36 +0530

    ADDED: Ability to add new contacts

--- src/bridgeit/addressbook/AddressBookMain.java
+++ src/bridgeit/addressbook/AddressBookMain.java
@@ -1,10 +1,31 @@
 package bridgeit.addressbook;
 
+import java.util.Scanner;
+
 public class AddressBookMain {
-
+	static int input;
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		do {
+			Scanner sc = new Scanner(System.in);
+			System.out.println("What do you want to do:\n1. Add Contact\n2. View Contact\n3. Exit");
+			input = sc.nextInt();
+		
+		
+				switch (input) {
+					case 1: {
+						ContactOperations contactOperations = ContactOperations.getInstance();
+						contactOperations.addNewContact();
+						break;
+					}
+		
+					case 2: {
+						ContactOperations contactOperations = ContactOperations.getInstance();
+						contactOperations.printAllContacts();
+						break;
+					}
+				}
+			}while(input != 3);
 	}
 
 }
